10a2a4b845a23a6a20c4d59029d9fdc4a01027b4
madhurima-thatavarthi <madhurima5798@gmail.com>
2021-01-22 11:02:02 +0530

    v1

--- src/main/java/com/Admin/controller/Controller.java
+++ src/main/java/com/Admin/controller/Controller.java
@@ -1,7 +1,6 @@
 package com.Admin.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,42 +10,54 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RestController;
 import com.Admin.model.Admin;
+import com.Admin.model.Donor;
 import com.Admin.service.Service;
+import com.Admin.model.Requestor;
 
+@RestController
 public class Controller {
 
 	@Autowired
-    Service service;
+	Service service;
 
-    @GetMapping(value = "/getadmins")
-    public ResponseEntity<List<Admin>> getAllAdmins() {
-        return new ResponseEntity<List<Admin>>(service.getAllAdmins(), HttpStatus.OK);
-    }
+	@GetMapping(value = "/getadmins")
+	public ResponseEntity<List<Admin>> getAllAdmins() {
+		return new ResponseEntity<List<Admin>>(service.getAllAdmins(), HttpStatus.OK);
+	}
 
-    @GetMapping(value = "/getadminbyid/{id}")
-    public ResponseEntity<Admin> getRequestorById(@PathVariable("id") int id) {
-        if (service.getAdminById(id) != null) {
-            return new ResponseEntity<Admin>(service.getAdminById(id), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<Admin>(HttpStatus.NO_CONTENT);
-        }
-    }
+	@GetMapping(value = "/getadminbyid/{id}")
+	public ResponseEntity<Admin> getRequestorById(@PathVariable("id") int id) {
+		if (service.getAdminById(id) != null) {
+			return new ResponseEntity<Admin>(service.getAdminById(id), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Admin>(HttpStatus.NO_CONTENT);
+		}
+	}
 
-    @PostMapping(value = "/createadmin")
-    public ResponseEntity<Admin> createRequestor(@RequestBody Admin admin) {
-        return new ResponseEntity<Admin>(service.createAdmin(admin), HttpStatus.CREATED);
-    }
+	@PostMapping(value = "/createadmin")
+	public ResponseEntity<Admin> createAdmin(@RequestBody Admin admin) {
+		return new ResponseEntity<Admin>(service.createAdmin(admin), HttpStatus.CREATED);
+	}
 
+	@PutMapping(value = "/updateadmin/{id}")
+	public ResponseEntity<Admin> updateRequestor(int id, @RequestBody Admin admin) {
+		return new ResponseEntity<Admin>(service.updateAdmin(id, admin), HttpStatus.CREATED);
+	}
 
-    @PutMapping(value = "/updateadmin/{id}")
-    public ResponseEntity<Admin> updateRequestor(int id, @RequestBody Admin admin) {
-        return new ResponseEntity<Admin>(service.updateAdmin(id, admin), HttpStatus.CREATED);
-    }
+	@DeleteMapping(value = "/deleteadmin/{id}")
+	public boolean deleteRequestor(@PathVariable("id") int id) {
+		return service.deleteAdmin(id);
+	}
 
-    @DeleteMapping(value = "/deleteadmin/{id}")
-    public boolean deleteRequestor(@PathVariable("id") int id) {
-        return service.deleteAdmin(id);
-    }
+	@GetMapping(value = "/approverequestor/{id}")
+	public ResponseEntity<Requestor> approveRequestor(@PathVariable("id") int id) {
+		return new ResponseEntity<Requestor>(service.approveRequestor(id), HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/approvedonor/{id}")
+	public ResponseEntity<Donor> approveDonor(@PathVariable("id") int id) {
+		return new ResponseEntity<Donor>(service.approveDonor(id), HttpStatus.OK);
+	}
 }
--- src/main/java/com/Admin/service/Service.java
+++ src/main/java/com/Admin/service/Service.java
@@ -1,19 +1,24 @@
 package com.Admin.service;
 
 import java.util.List;
-
 import com.Admin.model.Admin;
+import com.Admin.model.Donor;
+import com.Admin.model.Requestor;
 
 public interface Service {
 
 	public List<Admin> getAllAdmins();
-	
+
 	public Admin getAdminById(int id);
-	
-	public Admin createAdmin(Admin admin);
-	
-	public Admin updateAdmin(int id , Admin admin);
-	
+
+	public Admin updateAdmin(int id, Admin admin);
+
 	public boolean deleteAdmin(int id);
-	
+
+	public Requestor approveRequestor(int id);
+
+	public Donor approveDonor(int id);
+
+	public Admin createAdmin(Admin admin);
+
 }
